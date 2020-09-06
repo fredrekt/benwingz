2a927156f78f858b6eb3d621ae2223c911174b74
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-09-07 06:34:49 +0800

    initial content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,23 @@
 import React from "react"
 import Layout from "../components/layout"
-import { Button } from "baseui/button";
 import SEO from "../components/seo"
-import {Grid, Cell} from 'baseui/layout-grid';
+import Container from "../components/Container"
+import { StyledLink } from "baseui/link";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <Grid>
-      <Cell>
-        <h1>Heading 1</h1>
-        <h2>Heading 2</h2>
-        <h3>Heading 3</h3>
-        <h4>Heading 4</h4>
-        <h5>Heading 5</h5>
-        <h6>Heading 6</h6>
-        <Button>Button</Button>
-      </Cell>
-    </Grid>
+    <Container>
+      <h1>Base Web Starter</h1>
+      <p>A gatsby starter made by <StyledLink href="http://github.com/fredrekt">Fred Garingo</StyledLink>.</p>
+      <p>If you love Minimalistic Designs, then you're gonna love this starter. If you need to read more about the
+        documentation of Base Web - <StyledLink href="https://baseweb.design/">learn more here</StyledLink>. Base Web is made by Uber.
+      </p>
+      <p>Can be used for your Landing Page, Blog, Portfolio, E-Commerce and etc. Check out elemets to see the components, there are lots of 
+        components and elements from the documentation, <StyledLink href="https://baseweb.design/">see more here</StyledLink>.
+      </p>
+      <p>Base Web uses Styletron, It is an universal toolkit for component-oriented styling. It falls into the CSS in JS category. Styletron works great with React but can be used with other frameworks or plain JS as well.</p>
+    </Container>
   </Layout>
 )
 
