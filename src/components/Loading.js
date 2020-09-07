fb521cb6776027306687ce6f135dc3cabefa1f9f
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-09-07 08:37:05 +0800

    switched to text align

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,19 +1,11 @@
 import React from 'react'
 import ellipsis from '../images/ellipsis.gif'
-import {styled} from 'styletron-react';
 
 const Loading = () => {
-    const LoadingContainer = styled('div',{
-        display: `flex`,
-        position: `absolute`,
-        left: `0`,
-        right: `0`,
-        transform: `translate(-50%, -50%)`
-    })
     return (
-        <LoadingContainer>
+        <div style={{ textAlign: `center` }}>
            <img src={ellipsis} alt=""/>
-        </LoadingContainer>
+        </div>
     )
 }
 
