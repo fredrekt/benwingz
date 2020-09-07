811e5957257bd8959ab8f57b705915c043e5e556
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-09-07 08:24:44 +0800

    added page loader

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import ellipsis from '../images/ellipsis.gif'
+import {styled} from 'styletron-react';
 
 const Loading = () => {
+    const LoadingContainer = styled('div',{
+        display: `flex`,
+        position: `fixed`,
+        width: `100%`, height: `100%`,
+        textAlign: `center`,
+        justifyContent: `center`,
+        alignItems: `center`,
+        alignSelf: `center`
+    })
     return (
-        <div>
-           <p>loading</p> 
-        </div>
+        <LoadingContainer>
+           <img src={ellipsis} alt=""/>
+        </LoadingContainer>
     )
 }
 
