64f5d7a84e72aeb6592ed27e68bae1c9d4519165
Fredrick Garingo <fredrickjohng6@gmail.com>
2021-03-12 03:24:01 +0800

    done init design

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,60 @@
-import * as React from "react";
-import {
-  HeaderNavigation,
-  ALIGN,
-  StyledNavigationList,
-  StyledNavigationItem
-} from "baseui/header-navigation";
-import { StyledLink } from "baseui/link";
+import React from 'react';
+import { MDBNavbar, MDBNavbarNav, MDBNavbarToggler, MDBCollapse, MDBNavItem } from 'mdbreact';
+import { Link } from 'gatsby'
 
-export default () => {
-  return (
-    <HeaderNavigation style={{ padding: `1.5rem` }}>
-      <StyledNavigationList $align={ALIGN.left}>
-        <StyledNavigationItem>
-            <StyledLink animateUnderline href="/">
-                <h2>Gatsby Starter Base Web</h2>
-            </StyledLink>
-        </StyledNavigationItem>
-      </StyledNavigationList>
-      <StyledNavigationList $align={ALIGN.center} />
-      <StyledNavigationList style={{ marginRight: `1.5rem` }} $align={ALIGN.right}>
-        <StyledNavigationItem>
-          <StyledLink href="/about">
-            About
-          </StyledLink>
-        </StyledNavigationItem>
-        <StyledNavigationItem>
-          <StyledLink href="/elements">
-            Elements
-          </StyledLink>
-        </StyledNavigationItem>
-        <StyledNavigationItem>
-          <StyledLink href="/contact">
-            Contact
-          </StyledLink>
-        </StyledNavigationItem>
-      </StyledNavigationList>
-    </HeaderNavigation>
-  );
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapse: false,
+      isWideEnough: false,
+    };
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    this.setState({
+      collapse: !this.state.collapse,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <header>
+            <MDBNavbar color="bg-primary" fixed="top" className="p-3 z-depth-0" dark expand="md" scrolling transparent>
+              {!this.state.isWideEnough && <MDBNavbarToggler onClick={this.onClick} />}
+              <MDBCollapse isOpen={this.state.collapse} navbar>
+                <MDBNavbarNav center className="text-uppercase">
+                  <MDBNavItem className="px-4">
+                      <Link className="nav-link" to='/'>
+                        Home
+                      </Link>
+                  </MDBNavItem>
+                  <MDBNavItem className="px-4">
+                    <Link className="nav-link" to="/about">About</Link>
+                  </MDBNavItem>
+                  <MDBNavItem className="px-4">
+                    <Link className="nav-link" to='/menu'>Menu</Link>
+                  </MDBNavItem>
+                  <MDBNavItem className="px-4">
+                    <Link className="nav-link" to='/order'>Order</Link>
+                  </MDBNavItem>
+                  <MDBNavItem className="px-4">
+                    <Link className="nav-link" to='/contact'>Contact</Link>
+                  </MDBNavItem>
+                  <MDBNavItem className="px-4">
+                    <Link className="nav-link" to='/about#reviews'>Reviews</Link>
+                  </MDBNavItem>
+                </MDBNavbarNav>
+              </MDBCollapse>
+            </MDBNavbar>
+
+        </header>
+
+      </div>
+    );
+  }
 }
+
+export default Navbar;
